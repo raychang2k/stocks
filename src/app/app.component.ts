3fe70473762b25ebdee6f29a891bbf3c79f290dc
Ray Zhang <raychang2k@hotmail.com>
2018-08-12 13:54:39 +0800

    finish the summay components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { StockInterface, StocksService } from './services/stocks.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  stocks = Array<StockInterface>();
+
   title = 'stocks';
+
+  constructor(service: StocksService) {
+    service.load(['AAPL']).subscribe(stocks => {
+      this.stocks = stocks;
+    });
+  }
 }
